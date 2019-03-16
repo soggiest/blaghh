27e3be70790049a25b4d0a864178e0bbbbd2dcb9
soggiest <nicholas@heptio.com>
2019-03-16 15:27:55 -0700

    Added config changes

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'Soggy Newspaper', // Required
+  author: 'Nicholas Lane', // Required
+  description: 'Repository of neat things I learn',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://soggy.space',
+    github: 'https://github.com/soggiest',
+    twitter: 'https://twitter.com/apinick',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
 };
