7cd7d472b7aee4fe9e9e479f717dbbfe793f0f13
soggiest <nicholas@heptio.com>
2019-03-17 01:03:14 -0700

    more wrapper testing

--- src/components/HeaderImage/Wrapper.js
+++ src/components/HeaderImage/Wrapper.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   border: 5px solid #3598db;
   border-radius: 50%;
-  height: 150px;
+  height: 300px;
   margin: 0 auto 15px auto;
   overflow: hidden;
   width: 150px;
 
   img {
     padding: 5px;
-    border-radius: 75%;
+    border-radius: 50%;
   }
 `;
 
