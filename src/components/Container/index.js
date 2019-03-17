60e0aeacfcdc2c22ef1bf0e4207ead0f4c2e2d1f
soggiest <nicholas@heptio.com>
2019-03-17 01:49:36 -0700

    more testing

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.section`
 margin: 0 auto;
-max-width: 1000px;
+max-width: 870px;
 `;
 
 export default Container;
-// 870px
--- src/components/Header/Wrapper.js
+++ src/components/Header/Wrapper.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const Wrapper = styled.header`
   margin: 0 auto;
-  max-width: 650px;
+  max-width: 800px;
   padding: 0 50px 50px;
   text-align: center;
 `;
 
 export default Wrapper;
+//650px
