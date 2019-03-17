3699de30b0cbb850b28c15063700fb4bda6c7f30
soggiest <nicholas@heptio.com>
2019-03-17 01:51:45 -0700

    modified container

--- src/components/Header/Wrapper.js
+++ src/components/Header/Wrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.header`
   margin: 0 auto;
-  max-width: 800px;
+  max-width: 500px;
   padding: 0 50px 50px;
   text-align: center;
 `;
